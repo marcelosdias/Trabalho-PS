c807f3182b229dd8b0b89708e4d300dd38ffa8a6
marcelosdias <marcelodias741@hotmail.com>
2022-05-10 22:51:25 -0300

    Configurações iniciais da máquina

---
+++ Memory.java
@@ -0,0 +1,54 @@
+package projetoPS;
+
+public class Memory {
+	private Word[] memory;
+	private int size;
+	
+	public Memory (int memorySize) {
+		this.memory = new Word[memorySize];
+		this.size = memorySize;
+		
+		for (int i = 0; i < memorySize; i++)
+			this.memory[i] = new Word(8);
+		
+		this.resetMemory();
+	}
+	
+	// Retorna o objeto memória	
+	public Word[] getMemory() {
+		return this.memory;
+	}
+	
+	public void memoryWrite(int address, int numberBytes, Word value) {
+		for (int i = 0; i < numberBytes; i++) 
+			for (int j = 0, k = 0; j < 8; j++) 
+				this.memory[address + i].setBitByIndex(j, value.getValueByIndex(k++));
+	}
+	
+	// Listar toda a memória
+	public void listMemory() { // Debug
+		for (int i = 0; i < this.size; i++) {
+			System.out.print("Address " + i + ": ");
+			this.memory[i].listBits();
+		}
+	}
+	
+	// Retornar uma palavra
+	public void getWord(int address) {
+		int i;
+		for (i = address; i < (address + 3); i++) {
+			System.out.print("Address " + i + ": ");
+			this.memory[i].listBits();
+		}
+		
+	}
+	
+	public void resetMemory() {
+		for (int i = 0; i < this.size; i++) {
+			for (int j = 0; j < 8; j++)
+				this.memory[i].setBitByIndex(j, '0');
+		}
+	}
+}
+
+
---
+++ Word.java
@@ -0,0 +1,45 @@
+package projetoPS;
+
+public class Word {
+	private char[] bits;
+	private int size;
+	
+	public Word(int memoryLength) {
+		this.bits = new char[memoryLength];
+		this.size = memoryLength;
+	}
+
+	public void listBits() { // Debug
+		for (int i = 0; i < this.size; i++)
+			System.out.print(this.bits[i]);
+
+		System.out.println();
+	}
+	
+	public void setBits(char[] bits) {
+		this.bits = bits;
+	}
+	
+	public void setBitByIndex(int index, char value) {
+		this.bits[index] = value;
+	}
+	
+	public char[] getBits() {
+		return this.bits;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public char getValueByIndex(int index) {
+		return this.bits[index];
+	} 
+	
+	public int convertBinaryToDecimal() {
+		String convertedArray = new String(this.bits);
+		
+		return Integer.parseInt(convertedArray, 2);
+	}
+}
+
