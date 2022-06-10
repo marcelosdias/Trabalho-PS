6d2e0d3bcdf96f2a9a926a96ba3b9bebfa2d436b
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2022-06-10 18:00:35 -0300

    Integra interface com código

--- src/trabalho/ps/Register.java
+++ src/trabalho/ps/Register.java
@@ -1,24 +1,26 @@
-public class Register extends Word {
-	private int id;
-	private String name;
+package trabalho.ps;
 
-	public Register (int registerLength, int id, String name) {
-		super(registerLength);
-		this.id = id;
-		this.name = name;
-		
-		this.resetRegister();
-	}
-	
-	public void showRegister() {
-		System.out.println("Nome do Registrador: " + this.name);
-		System.out.println("Identificador: " + this.id);
-		System.out.print("Conteudo: ");
-		super.listBits();
-	}
-	
-	public void resetRegister() {
-		for (int i = 0; i < super.getSize(); i++)
-			super.setBitByIndex(i, '0');
-	}
+public class Register extends Word{
+    private int id;
+    private String name;
+
+    public Register (int registerLength, int id, String name) {
+        super(registerLength);
+        this.id = id;
+        this.name = name;
+
+        this.resetRegister();
+    }
+
+    public void showRegister() {
+        System.out.println("Nome do Registrador: " + this.name);
+        System.out.println("Identificador: " + this.id);
+        System.out.print("Conteudo: ");
+        super.listBits();
+    }
+
+    public void resetRegister() {
+        for (int i = 0; i < super.getSize(); i++)
+                super.setBitByIndex(i, '0');
+    }
 }
--- src/trabalho/ps/Word.java
+++ src/trabalho/ps/Word.java
@@ -1,71 +1,71 @@
+package trabalho.ps;
+
 public class Word {
-	private char[] bits;
-	private int size;
-	
-	public Word(int memoryLength) {
-		this.bits = new char[memoryLength];
-		this.size = memoryLength;
-	}
+    private char[] bits;
+    private int size;
 
-	public void listBits() { // Debug
-		for (int i = 0; i < this.size; i++)
-			System.out.print(this.bits[i]);
+    public Word(int memoryLength) {
+            this.bits = new char[memoryLength];
+            this.size = memoryLength;
+    }
 
-		System.out.println();
-	}
-	
-	public void setBits(char[] bits) {
-		this.bits = bits;
-	}
-	
-  public void setBits(int value) { 
-		String stringValue = Integer.toBinaryString(value);
+    public void listBits() { // Debug
+            for (int i = 0; i < this.size; i++)
+                    System.out.print(this.bits[i]);
 
-		int valueLength = stringValue.length();
+            System.out.println();
+    }
 
-		if (valueLength < this.getSize()) {
-			String valueAux = String.format("%" + this.getSize() + "s", stringValue).replace(" ", "0");
+    public void setBits(char[] bits) {
+            this.bits = bits;
+    }
 
-			this.bits = valueAux.toCharArray();
-		}
-		else 
-				this.bits = Integer.toBinaryString(value).toCharArray();
-  }
+    public void setBits(int value) { 
+            String stringValue = Integer.toBinaryString(value);
 
-	public void setBitByIndex(int index, char value) {
-		this.bits[index] = value;
-	}
-	
-	public char[] getBits() {
-		return this.bits;
-	}
-	
-	public int getSize() {
-		return this.size;
-	}
-	
-	public char getValueByIndex(int index) {
-		return this.bits[index];
-	} 
-	
-	public int convertBinaryToDecimal() {
-		int decimal = 0;
+            int valueLength = stringValue.length();
 
-        for(int index = 0; index < bits.length; index++)
-            if (bits[index] == '1') decimal += Math.pow(2, (bits.length - index - 1));
-        
-		return decimal;
-	}
-	
-	public String convertBinaryToHex() {
-		int decimal = convertBinaryToDecimal();
-		
-		return Integer.toHexString(decimal).toLowerCase();
-	}
-	
-	public int getFormat() {
-		return this.size / 8;
-	}
+            if (valueLength < this.getSize()) {
+                    String valueAux = String.format("%" + this.getSize() + "s", stringValue).replace(" ", "0");
+
+                    this.bits = valueAux.toCharArray();
+            }
+            else 
+                            this.bits = Integer.toBinaryString(value).toCharArray();
+    }
+
+    public void setBitByIndex(int index, char value) {
+            this.bits[index] = value;
+    }
+
+    public char[] getBits() {
+            return this.bits;
+    }
+
+    public int getSize() {
+            return this.size;
+    }
+
+    public char getValueByIndex(int index) {
+            return this.bits[index];
+    } 
+
+    public int convertBinaryToDecimal() {
+            int decimal = 0;
+
+    for(int index = 0; index < bits.length; index++)
+        if (bits[index] == '1') decimal += Math.pow(2, (bits.length - index - 1));
+
+            return decimal;
+    }
+
+    public String convertBinaryToHex() {
+            int decimal = convertBinaryToDecimal();
+
+            return Integer.toHexString(decimal).toLowerCase();
+    }
+
+    public int getFormat() {
+            return this.size / 8;
+    }
 }
-
-
